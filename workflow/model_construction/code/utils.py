245e2123b74d5f51809f596ccaa3a7bee2ac2c98
David Lafferty <davidcl2@illinois.edu>
2022-09-13 14:01:11 -0400

    initial usda data downloads and updated README

---
+++ workflow/model_construction/code/utils.py
@@ -0,0 +1,15 @@
+##################################################################
+# utils files containing some common variable definitions
+##################################################################
+
+state_names = ['Arkansas', 'Illinois', 'Indiana', 'Iowa', 'Kentucky', 'Kansas', 
+                 'Oklahoma', 'South Dakota', 'North Dakota',  'Michigan', 'Tenessee',
+                 'Minnesota', 'Missouri', 'Ohio', 'Wisconsin', 'Nebraska']
+
+state_names_short = ['AR', 'IL', 'IN', 'IA', 'KY', 'KS', 
+                       'OK', 'SD', 'ND',  'MI', 'TN',
+                       'MN', 'MO', 'OH', 'WI', 'NE']
+
+state_alphas = ['05', '17', '18', '19', '21', '20', 
+                  '40', '46', '48',  '26', '47',
+                  '27', '29', '39', '55', '31']
