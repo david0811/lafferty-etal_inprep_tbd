b70bf3a401f6521db67a8f6213eb70c1f164e454
David Lafferty <davidcl2@illinois.edu>
2022-09-15 12:33:52 -0400

    update README

---
+++ workflow/model_construction/code/00b_gmfd_wget.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# mkdir ../input_data/gmfd_raw
+# cd ../input_data/gmfd_raw
+
+mkdir /gpfs/group/kaf26/default/dcl5300/gmfd_tas
+cd /gpfs/group/kaf26/default/dcl5300/gmfd_tas
+
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1948-1948.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1949-1949.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1950-1950.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1951-1951.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1952-1952.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1953-1953.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1954-1954.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1955-1955.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1956-1956.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1957-1957.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1958-1958.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1959-1959.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1960-1960.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1961-1961.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1962-1962.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1963-1963.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1964-1964.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1965-1965.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1966-1966.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1967-1967.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1968-1968.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1969-1969.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1970-1970.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1971-1971.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1972-1972.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1973-1973.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1974-1974.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1975-1975.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1976-1976.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1977-1977.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1978-1978.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1979-1979.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1980-1980.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1981-1981.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1982-1982.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1983-1983.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1984-1984.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1985-1985.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1986-1986.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1987-1987.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1988-1988.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1989-1989.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1990-1990.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1991-1991.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1992-1992.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1993-1993.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1994-1994.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1995-1995.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1996-1996.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1997-1997.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1998-1998.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_1999-1999.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_2000-2000.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_2001-2001.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_2002-2002.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_2003-2003.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_2004-2004.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_2005-2005.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_2006-2006.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_2007-2007.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_2008-2008.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_2009-2009.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_2010-2010.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_2011-2011.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_2012-2012.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_2013-2013.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_2014-2014.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_2015-2015.nc
+wget -nc -c -nd http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/tas_daily_2016-2016.nc
